3c85aca361e89c58584ff8a1cd1dca22dd3dbe1b
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-12 17:39:34 +0100

    initialize models

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from cursos.entities.curso import Curso
+from cursos.entities.avaliacao import Avaliacao
 
 # Create your models here.
+
+Curso(models)
+Avaliacao(models)
