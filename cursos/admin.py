a0cd08e1bc2e50b0aa9062101a0983e2b5274c1d
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-12 17:39:21 +0100

    register models

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from cursos.entities import *
 
 # Register your models here.
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo')
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('curso', 'nome', 'email', 'avaliacao', 'criacao', 'atualizacao', 'ativo')
