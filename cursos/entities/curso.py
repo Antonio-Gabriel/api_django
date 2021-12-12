12e46d69b48d409ccc198ed92c2d52b38a6060f0
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-12 17:39:56 +0100

    create model for course

---
+++ cursos/entities/curso.py
@@ -0,0 +1,13 @@
+from django.db import models
+from .base import Base
+
+class Curso(Base, models.Model):
+    titulo = models.CharField(max_length=255)
+    url = models.URLField(unique=True)
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+    
+    def __str__(self):
+        return self.titulo
