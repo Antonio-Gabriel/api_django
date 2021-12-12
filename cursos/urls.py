58e05dca043df0e9adfb3210543e30026486e1dd
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-12 22:50:44 +0100

    add new routes

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 
-from .views import CursoViewAPI, AvaliacaoViewAPI
+from .views import CursoAPIView, AvaliacaoAPIView, AvaliacoesAPIView, CursosAPIView
 
 urlpatterns = [
-    path('cursos/', CursoViewAPI().as_view(), name="cursos"),
-    path('avaliacoes/', AvaliacaoViewAPI().as_view(), name="avaliacoes"),
+    path('cursos/', CursosAPIView().as_view(), name="cursos"),
+    path('cursos/<int:pk>/', CursoAPIView().as_view(), name="curso"),
+
+    path(
+        'cursos/<int:curso_pk>/avaliacoes', 
+        AvaliacoesAPIView().as_view(), name="curso_avaliacoes"),
+    
+    path(
+        'cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>', 
+        AvaliacaoAPIView().as_view(), name="curso_avaliacoes"),
+
+    path('avaliacoes/', AvaliacoesAPIView().as_view(), name="avaliacoes"),
+    path('avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView().as_view(), name="avaliacao"),
 ]
