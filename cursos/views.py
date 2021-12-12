72a1c286e97cfb261e6b042c4b442629042bd57d
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-12 20:15:26 +0100

    Views api

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from cursos.entities import *
+from cursos.api import *
+
+
+class CursoViewAPI(APIView):
+    """
+        API de cursos
+    """
+
+    def get(self, request) -> Response:        
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoViewAPI(APIView):
+    """
+        API de avaliações
+    """
+
+    def get(self, request) -> Response:
+        avaliacao = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacao, many=True)
+        return Response(serializer.data)
