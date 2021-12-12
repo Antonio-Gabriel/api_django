6d9914b31d36fcc35c1f5f726945939b20aecbbd
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-12 17:39:49 +0100

    create models for avaliacao

---
+++ cursos/entities/avaliacao.py
@@ -0,0 +1,21 @@
+from django.db import models
+from .base import Base
+
+from .curso import Curso
+
+class Avaliacao(Base, models.Model):
+    curso = models.ForeignKey(Curso, related_name='avaliacoes', on_delete=models.CASCADE)
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    comentario = models.TextField(blank=True, default='')
+    avaliacao = models.DecimalField(max_digits=2, decimal_places=1) # Para poder usar 4.1, 1.8
+
+    class Meta:
+        verbose_name = "Avaliação"
+        verbose_name_plural = "Avaliações"
+
+        # Restringir para o usuários avaliar unicamente um curso
+        unique_together = ['email', 'curso'] 
+
+    def __str__(self):
+        return f'{self.nome} avaliou o curso {self.curso} com nota {self.avaliacao}'
