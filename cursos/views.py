e388dca3958f28fb320ebe67bba2781fb774305d
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-12 22:51:12 +0100

    views using generics

--- cursos/views.py
+++ cursos/views.py
@@ -1,27 +1,39 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from cursos.entities import *
 from cursos.api import *
 
+class CursosAPIView(generics.ListCreateAPIView):
+    queryset=Curso.objects.all()
+    serializer_class = CursoSerializer
 
-class CursoViewAPI(APIView):
-    """
-        API de cursos
-    """
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Curso.objects.all()
+    serializer_class = CursoSerializer
 
-    def get(self, request) -> Response:        
-        cursos = Curso.objects.all()
-        serializer = CursoSerializer(cursos, many=True)
-        return Response(serializer.data)
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset=Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get("curso_id"):
+            return self.queryset.filter(curso_id=self.kwargs.get("curso_id"))
+        return self.queryset.all()
 
-class AvaliacaoViewAPI(APIView):
-    """
-        API de avaliações
-    """
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset=Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer    
 
-    def get(self, request) -> Response:
-        avaliacao = Avaliacao.objects.all()
-        serializer = AvaliacaoSerializer(avaliacao, many=True)
-        return Response(serializer.data)
+    def get_object(self):
+        if self.kwargs.get("curso_pk"):
+            return get_object_or_404(
+                    self.get_queryset(), 
+                    curso_id=self.kwargs.get("curso_pk"), 
+                    pk=self.kwargs.get("avaliacao_pk")
+                    )
+
+        return get_object_or_404(
+                self.get_queryset(),                 
+                pk=self.kwargs.get("avaliacao_pk")
+                )
