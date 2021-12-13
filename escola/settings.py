4338ad112367b9a6b8e469353da195b53514906a
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-13 16:02:20 +0100

    configure authToken

--- escola/settings.py
+++ escola/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'django_filters',
     'rest_framework',
+    'rest_framework.authtoken',
 
     'cursos'
 ]
@@ -136,8 +137,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
+        
+        # Usuário consegur fazer apenas leitura caso não esteje autenticado
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )    
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4,    
 }
